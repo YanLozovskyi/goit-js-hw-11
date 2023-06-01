b7596562ce6246fb8aa45c38980b08ec5dc04d94
YanLozovskyi <yanlozovskiy@gmail.com>
2023-06-01 18:17:09 +0300

    up

--- src/index.js
+++ src/index.js
@@ -87,11 +87,17 @@ async function onLoadMore() {
   }
 }
 
-window.onscroll = function () {
+// fixed header
+
+function onScrollHeader(px) {
   const screenSize = window.screen.width;
-  if (screenSize >= 768) {
+  if (screenSize >= px) {
     scrollY <= 70
       ? document.getElementById('header').classList.remove('header-scroll')
       : document.getElementById('header').classList.add('header-scroll');
   }
+}
+
+window.onscroll = function () {
+  onScrollHeader(768);
 };
