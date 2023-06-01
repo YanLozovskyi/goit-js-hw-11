caa7a4a811f241d0979df81990ecf98ef7201cfd
YanLozovskyi <yanlozovskiy@gmail.com>
2023-06-01 18:02:04 +0300

    add fixed header

--- src/index.js
+++ src/index.js
@@ -86,3 +86,15 @@ async function onLoadMore() {
     console.log(error);
   }
 }
+
+window.onscroll = function () {
+  if (window.screen.width >= 768) {
+    scrollY <= 70
+      ? document.getElementById('header').classList.remove('header-scroll')
+      : document.getElementById('header').classList.add('header-scroll');
+  } else {
+    scrollY <= 120
+      ? document.getElementById('header').classList.remove('header-scroll')
+      : document.getElementById('header').classList.add('header-scroll');
+  }
+};
